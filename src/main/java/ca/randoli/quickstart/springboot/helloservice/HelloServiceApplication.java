fe2af9f3f92ff234c73196a03fc5e9125f3aba59
TharakiPanabokkeRandoliAcc <79511759+TharakiPanabokkeRandoliAcc@users.noreply.github.com>
2021-02-25 19:01:26 +0530

    Update HelloServiceApplication.java

--- src/main/java/ca/randoli/quickstart/springboot/helloservice/HelloServiceApplication.java
+++ src/main/java/ca/randoli/quickstart/springboot/helloservice/HelloServiceApplication.java
@@ -13,9 +13,14 @@ public class HelloServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloServiceApplication.class, args);
 	}
+	
+	@GetMapping("/")
+	public String home(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello Mr %s! This is Hello Service War/java11", name);
+	}
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello Mr %s! This is Hello Service War/java11", name);
+		return String.format("Hello Mr %s! This is a route in Hello Service War/java11", name);
 	}
 }
